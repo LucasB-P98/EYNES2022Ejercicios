d42682f7e4a206d0c18f2504ca3b28667ccae046
Lucas loiza <lucasnloiza@gmail.com>
2022-05-11 13:03:02 -0300

    Finalizado

--- EjercicioMatriz/EjercicioMatriz.py
+++ EjercicioMatriz/EjercicioMatriz.py
@@ -1,8 +1,23 @@
 import numpy as np
 import random
+import doctest
 
 
 def matrix_creation(row,col):
+	'''
+	Matrix creation function returns a matrix
+
+	>>> matrix_creation(3,3)
+	[[2,4,7]
+ 	[5,7,2]
+ 	[4,10,3]
+	 [1,8,8]]
+ 	>>> matrix_creation(0,0)
+	 []
+	 >>> matrix_creation(a,b)
+ 	NameError
+
+	'''
 	return [[random.randint(1,10)for j in range(col)]for i in range(row)]
 
 
@@ -10,24 +25,27 @@ if __name__ == "__main__":
 
 	matrix=matrix_creation(5,5)
 
-	print("Original Matrix")
+	print("-Original Matrix-\n")
 
-	for row in matrix:
-		print(row)
+	print(np.transpose(matrix))
+
+	found_initial_position=0
+	found_final_position=0
+	consecutive_sequence=0
 
 	i=0
 	j=0
 
-	
-	consecutive_sequence_horizontal=0
-	consecutive_sequence_vertical=0
-
 	for i in range(len(matrix)):
-		print(matrix[i][j])
-		for j in range(len(matrix)):
-			pass
+		
+		for j in range(len(matrix)-1):
+			if ((matrix[i][j]-matrix[i][j+1])== 1 or (matrix[i][j]-matrix[i][j+1])== -1):
+				if consecutive_sequence==0:
+					found_initial_position=(i,j)
+				consecutive_sequence+=1
+				found_final_position=(i,j)
 
-	print("The vertical consecutive numbers sequence is: "+str(consecutive_sequence_vertical))
+	print("\nThe consecutive numbers sequence is: "+str(consecutive_sequence)+" \nInitial position at: "+str(found_initial_position)+"\nFinal position at : "+str(found_final_position))
 
 
 
