efe20f6ff4aadf5f46e2a79f7481291a59c0cb6e
Lucas loiza <lucasnloiza@gmail.com>
2022-05-11 13:35:38 -0300

    Finalizado Ejercicio Simple

--- EjercicioSimple/EjercicioSimple.py
+++ EjercicioSimple/EjercicioSimple.py
@@ -2,57 +2,41 @@ import doctest
 import random
 from operator import itemgetter
 
-def lista_dic():
-	listadic=[
-		{
-			'id':1,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':2,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':3,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':4,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':5,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':6,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':7,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':8,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':9,
-			'edad':random.randint(1,100)
-		},
-		{
-			'id':10,
-			'edad':random.randint(1,100)
-		},
-]
-	return listadic
+def create_lista_dic():
+	'''
+	This function creates and returns a list with dictionaries/persons with IDs in range 10 and randomized ages
+
+	'''	
+	list_dic=[]
+
+	for i in range(10):
+		auxiliar_dic={	'id':i+1,
+						'age':random.randint(1,100)}
+		list_dic.append(auxiliar_dic)
+
+	return list_dic
 
 
-def ordenar_clientes_edad(lista):
+def client_per_age(list_dic):
+	'''
+	This function receives a list and returns the same list but organized per age
 
-	lista_ordenada = sorted(lista, key=itemgetter('edad'),reverse=True)
+	>>> client_per_age(list_dic= [{'id':4,'age':80}, {'id':1,'age':60}, {'id':8,'age':10}...])
+	order_list[{'id':4,'age':80}, {'id':1,'age':60}, {'id':8,'age':10}...]
+	'''
+	order_list = sorted(list_dic, key=itemgetter('age'),reverse=True)
 
-	print("La persona mas joven tiene la id: "+str(lista_ordenada[9]['id']))
-	print("La persona mas vieja tiene la id: "+str(lista_ordenada[0]['id']))
+	return order_list
 
-	return lista_ordenada
+if __name__ == "__main__":
+	my_list=create_lista_dic()
+	my_list_size=len(my_list)
+
+	print("Original list:\n\n"+str(my_list))
+
+	my_list_per_Age=client_per_age(my_list)
+
+	print("\n\nList per Age:\n\n"+str(my_list_per_Age))
+
+	print("\n\nThe youngest person has the id: "+str(my_list_per_Age[my_list_size-1]['id']))
+	print("The oldest person has the id: "+str(my_list_per_Age[my_list_size-my_list_size]['id'])+"\n")
